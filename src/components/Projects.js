8274ed4523c8cfb2fb7f73a7d369eb727359b868
Eden Sahlie <edensahlie19@gmail.com>
2026-06-25 13:13:41 +0300

    addition of project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,6 @@
 import studentRoster from "../assets/studentRoster.png"
+import pillSafe from "../assets/pillSafe.png"
 export const Projects = [
-    {id:1, img : studentRoster, name : "Student Roster", link:"https://github.com/Eden1916/Student_Roster.git", description:"It is used to manage student information including adding, viewing, updating, and deleting student records."}
+    {id:1, img : studentRoster, name : "Student Roster", link:"https://eden.freehosting.dev/", description:"It is used to manage student information including adding, viewing, updating, and deleting student records."},
+    {id:2, img : pillSafe, name : "Pill Safe", link:"https://daw-delta.vercel.app/", description:"It is used to know and understand the purpose of medicines we are using and if they have side effect thet we should be aware of. And users can ask and get information about medicines in three languages; English, Amharic and Afan Oromoo. Additionally users can ask about the medicines by uploading a picture of the medicine."}
 ]
