a98999e7fb4004845d5dc13f4252e315bc3e5ad3
Eden Sahlie <edensahlie19@gmail.com>
2026-06-25 15:01:20 +0300

    some change to make it flexable

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -23,7 +23,9 @@ export default function Portfolio({ theme, setTheme }) {
     try {
       console.log("Submitting form", formData);
 
-      const res = await fetch("http://localhost:5000/api/contact", {
+      const API_URL= import.meta.env.VITE_API_URL || "http://localhost:5000";
+
+      const res = await fetch(`${API_URL}/api/contact`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
